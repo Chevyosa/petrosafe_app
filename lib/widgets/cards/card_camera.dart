f8ed020c102b4b8932d4e9f23e51f72b4be7a814
Riyanda Azis Febrian <riyandaazis00@gmail.com>
2025-10-30 09:10:38 +0700

    Added Camera Feature to CameraCard

--- lib/widgets/cards/card_camera.dart
+++ lib/widgets/cards/card_camera.dart
@@ -1,48 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CameraCard extends StatelessWidget {
   final String targetFoto;
   final String sisiFoto;
   final String tujuanFoto;
+  final Function(XFile?)? onImageTaken;
 
   const CameraCard({
     super.key,
     required this.targetFoto,
     required this.sisiFoto,
     required this.tujuanFoto,
+    this.onImageTaken,
   });
+
+  Future<void> _openCamera(BuildContext context) async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.camera);
+
+    if (onImageTaken != null) {
+      onImageTaken!(image);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     const cameraIcon = Icon(FeatherIcons.camera, color: Colors.white);
     return SizedBox(
       width: double.infinity,
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadiusGeometry.circular(20),
-        ),
-        color: Colors.red[800],
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            children: <Widget>[
-              cameraIcon,
-              SizedBox(height: 10),
-              Text(
-                "Ambil $targetFoto",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(20),
+        onTap: () => _openCamera(context),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusGeometry.circular(20),
+          ),
+          color: Colors.red[800],
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: <Widget>[
+                cameraIcon,
+                SizedBox(height: 10),
+                Text(
+                  "Ambil $targetFoto",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                "Foto Bagian $sisiFoto Yang $tujuanFoto",
-                style: TextStyle(color: Colors.white),
-              ),
-            ],
+                SizedBox(height: 10),
+                Text(
+                  "Foto Bagian $sisiFoto Yang $tujuanFoto",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
           ),
         ),
       ),
