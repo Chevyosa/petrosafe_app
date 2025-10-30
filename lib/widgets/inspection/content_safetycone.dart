50fe2e9510720ec40fe3fe7a99a3b1987afdd8f5
Riyanda Azis Febrian <riyandaazis00@gmail.com>
2025-10-30 19:23:12 +0700

    Created Safety Cone Content

--- lib/widgets/inspection/content_safetycone.dart
+++ lib/widgets/inspection/content_safetycone.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:petrosafe_app/widgets/cards/card_conformity.dart';
+import 'package:petrosafe_app/widgets/forms/form_inspection.dart';
 
 class SafetyConeContent extends StatelessWidget {
   const SafetyConeContent({super.key});
@@ -7,12 +9,44 @@ class SafetyConeContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Cek Safety Cone", style: TextStyle(fontSize: 20)),
+        title: const Text("Cek Safety Cone", style: TextStyle(fontSize: 20)),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
       ),
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(children: <Widget>[]),
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text("Jumlah Tersedia"),
+              const PetroForm(hintText: "Masukkan Jumlah Tersedia"),
+              const SizedBox(height: 8),
+              const ConformityCard(),
+
+              Spacer(),
+
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                  ),
+                  onPressed: () => {Navigator.pop(context)},
+                  child: Text(
+                    "Simpan",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
