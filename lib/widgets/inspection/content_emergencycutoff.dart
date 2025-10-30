dd50811c62580cc9598d552d2c9229206283c6ef
Riyanda Azis Febrian <riyandaazis00@gmail.com>
2025-10-30 19:39:44 +0700

    Created Emergency Cut Off Content

--- lib/widgets/inspection/content_emergencycutoff.dart
+++ lib/widgets/inspection/content_emergencycutoff.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:petrosafe_app/widgets/cards/card_conformity.dart';
+import 'package:petrosafe_app/widgets/cards/card_functionality.dart';
+import 'package:petrosafe_app/widgets/forms/form_inspection.dart';
 
 class EmergencyCutOffContent extends StatelessWidget {
   const EmergencyCutOffContent({super.key});
@@ -7,12 +10,49 @@ class EmergencyCutOffContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Cek Emergency Cut Off", style: TextStyle(fontSize: 20)),
+        title: const Text(
+          "Cek Emergency Cut Off",
+          style: TextStyle(fontSize: 20),
+        ),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
       ),
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(children: <Widget>[]),
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text("Jumlah"),
+              const PetroForm(hintText: "Masukkan Jumlah"),
+              const SizedBox(height: 8),
+              FuncitonalityCard(),
+              const SizedBox(height: 8),
+              const ConformityCard(),
+
+              Spacer(),
+
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                  ),
+                  onPressed: () => {Navigator.pop(context)},
+                  child: Text(
+                    "Simpan",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
