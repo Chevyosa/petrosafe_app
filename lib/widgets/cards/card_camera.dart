23f08e8105fee3fbfadd05d10d62a8df535085d1
Riyanda Azis Febrian <riyandaazis00@gmail.com>
2025-11-01 10:23:39 +0700

    Added Photo Preview After Taking Photo

--- lib/widgets/cards/card_camera.dart
+++ lib/widgets/cards/card_camera.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class CameraCard extends StatelessWidget {
+class CameraCard extends StatefulWidget {
   final String targetFoto;
   final String sisiFoto;
   final String tujuanFoto;
@@ -16,18 +17,49 @@ class CameraCard extends StatelessWidget {
     this.onImageTaken,
   });
 
+  @override
+  State<CameraCard> createState() => _CameraCardState();
+}
+
+class _CameraCardState extends State<CameraCard> {
+  XFile? _capturedImage;
+
   Future<void> _openCamera(BuildContext context) async {
     final ImagePicker picker = ImagePicker();
     final XFile? image = await picker.pickImage(source: ImageSource.camera);
 
-    if (onImageTaken != null) {
-      onImageTaken!(image);
+    if (image != null) {
+      setState(() => _capturedImage = image);
+      widget.onImageTaken?.call(image);
     }
+  }
+
+  void _showImagePreview(BuildContext context, File imageFile) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (_) => GestureDetector(
+        onTap: () => Navigator.pop(context),
+        child: Container(
+          color: Colors.black.withOpacity(0.9),
+          child: Center(
+            child: Hero(
+              tag: "preview_${widget.targetFoto}",
+              child: InteractiveViewer(
+                panEnabled: true,
+                minScale: 0.8,
+                maxScale: 4.0,
+                child: Image.file(imageFile),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    const cameraIcon = Icon(FeatherIcons.camera, color: Colors.white);
     return SizedBox(
       width: double.infinity,
       child: InkWell(
@@ -35,28 +67,73 @@ class CameraCard extends StatelessWidget {
         onTap: () => _openCamera(context),
         child: Card(
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadiusGeometry.circular(20),
+            borderRadius: BorderRadius.circular(20),
           ),
           color: Colors.red[800],
           child: Padding(
-            padding: EdgeInsets.all(16),
+            padding: const EdgeInsets.all(16),
             child: Column(
-              children: <Widget>[
-                cameraIcon,
-                SizedBox(height: 10),
-                Text(
-                  "Ambil $targetFoto",
-                  style: TextStyle(
+              children: [
+                if (_capturedImage == null) ...[
+                  const Icon(
+                    FeatherIcons.camera,
                     color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
+                    size: 48,
                   ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  "Foto Bagian $sisiFoto Yang $tujuanFoto",
-                  style: TextStyle(color: Colors.white),
-                ),
+                  const SizedBox(height: 10),
+                  Text(
+                    "Ambil ${widget.targetFoto}",
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    "Foto Bagian ${widget.sisiFoto} Yang ${widget.tujuanFoto}",
+                    style: const TextStyle(color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
+                ] else ...[
+                  GestureDetector(
+                    onTap: () =>
+                        _showImagePreview(context, File(_capturedImage!.path)),
+                    child: Hero(
+                      tag: "preview_${widget.targetFoto}",
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Image.file(
+                          File(_capturedImage!.path),
+                          fit: BoxFit.cover,
+                          height: 180,
+                          width: double.infinity,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    "Foto ${widget.targetFoto} berhasil diambil",
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(height: 6),
+                  TextButton.icon(
+                    onPressed: () => _openCamera(context),
+                    icon: const Icon(
+                      FeatherIcons.refreshCw,
+                      color: Colors.white,
+                      size: 16,
+                    ),
+                    label: const Text(
+                      "Ambil Ulang",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
               ],
             ),
           ),
